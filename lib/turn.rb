9d9ea898406a098bbd98a087708f612b93b413a0
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-10 15:52:03 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,5 +23,12 @@ def position_taken?(board, index)
 end
 
 def turn(board)
- puts "Please enter 1-9:" 
+ puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(array, index, player)
+  else "Move Invalid"
+    turn(board)
+  end 
 end
